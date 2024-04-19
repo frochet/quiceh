df7d835e1b41d858ea1ac6195c3aeabc0430219c
Evan Rittenhouse <erittenhouse@cloudflare.com>
2024-04-18 19:29:16 -0500

    Finish recv_from

--- dgram/src/buffer.rs
+++ dgram/src/buffer.rs
@@ -1,8 +1,15 @@
+use std::fs::metadata;
+use std::io::IoSlice;
+use std::io::IoSliceMut;
 use std::io::Result;
+use std::net::SocketAddr;
+use std::net::SocketAddrV4;
+use std::net::SocketAddrV6;
 use std::os::fd::AsRawFd;
 use std::time::Instant;
+use std::time::SystemTime;
 
-// TODO: test multi-platform bits
+// TODO: test multi-platform bits, ensure linux/non-linux pieces are separate
 
 #[cfg(all(target_os = "linux", not(fuzzing)))]
 mod linux_imports {
@@ -13,6 +20,13 @@ mod linux_imports {
     pub(super) use smallvec::SmallVec;
 }
 
+use libc::c_int;
+use nix::errno::Errno;
+use nix::sys::socket::recvmsg;
+use nix::sys::socket::AddressFamily;
+use nix::sys::socket::ControlMessageOwned;
+use nix::sys::socket::SockaddrStorage;
+
 #[cfg(all(target_os = "linux", not(fuzzing)))]
 use self::linux_imports::*;
 
@@ -21,13 +35,13 @@ mod non_linux_imports {
     pub(super) use std::net::SocketAddr;
 }
 
-pub const UDP_MAX_GSO_PACKET_SIZE: usize = 65507;
+const UDP_MAX_GSO_PACKET_SIZE: usize = 65507;
 
 #[cfg(any(not(target_os = "linux"), fuzzing))]
 use self::non_linux_imports::*;
 
 #[cfg(any(not(target_os = "linux"), fuzzing))]
-fn send_to(
+pub fn send_to(
     fd: impl AsRawFd, send_buf: &[u8], segment_size: usize, num_pkts: usize,
     tx_time: Option<Instant>, client_addr: Option<SocketAddr>,
 ) -> Result<usize> {
@@ -42,7 +56,7 @@ fn send_to(
 }
 
 #[cfg(all(target_os = "linux", not(fuzzing)))]
-fn send_to<S>(
+pub fn send_to<S>(
     fd: impl AsRawFd, send_buf: &[u8], segment_size: usize, num_pkts: usize,
     tx_time: Option<Instant>, client_addr: Option<&S>,
 ) -> Result<usize>
@@ -55,7 +69,7 @@ where
 
     let now = Instant::now();
 
-    let iov = [std::io::IoSlice::new(send_buf)];
+    let iov = [IoSlice::new(send_buf)];
     let segment_size_u16 = segment_size as u16;
 
     let raw_time = tx_time
@@ -74,8 +88,83 @@ where
         cmsgs.push(ControlMessage::TxTime(&raw_time));
     }
 
-    return sendmsg(fd.as_raw_fd(), &iov, &cmsgs, MsgFlags::empty(), client_addr)
-        .map_err(Into::into);
+    sendmsg(fd.as_raw_fd(), &iov, &cmsgs, MsgFlags::empty(), client_addr)
+        .map_err(Into::into)
+}
+
+/// Output of a `recvmsg` call
+pub struct RecvData {
+    pub bytes: usize,
+    pub peer_addr: Option<SocketAddr>,
+    pub rx_time: Option<SystemTime>,
+    pub gro: Option<u16>,
+}
+
+/// Receive a message via `recvmsg`.
+///
+/// # Note
+///
+/// It is the caller's responsibility to create and clear the cmsg space. `nix`
+/// recommends that the space be created via the `cmsg_space!()` macro.
+pub fn recv_from(
+    fd: impl AsRawFd, read_buf: &mut [u8], cmsg_space: &mut Vec<u8>,
+    msg_flags: Option<MsgFlags>,
+) -> std::result::Result<RecvData, Errno> {
+    cmsg_space.clear();
+
+    let iov_s = &mut [IoSliceMut::new(read_buf)];
+    let msg_flags = msg_flags.unwrap_or(MsgFlags::empty());
+
+    match recvmsg::<SockaddrStorage>(
+        fd.as_raw_fd(),
+        iov_s,
+        Some(cmsg_space),
+        msg_flags,
+    ) {
+        Ok(r) => {
+            println!("{:?}", r);
+            let bytes = r.bytes;
+
+            let address = match r.address {
+                Some(a) => a,
+                _ => return Err(Errno::EINVAL.into()),
+            };
+
+            let peer_addr = match address.family() {
+                Some(AddressFamily::Inet) => Some(
+                    SocketAddrV4::from(*address.as_sockaddr_in().unwrap()).into(),
+                ),
+                Some(AddressFamily::Inet6) => Some(
+                    SocketAddrV6::from(*address.as_sockaddr_in6().unwrap())
+                        .into(),
+                ),
+                _ => None,
+            };
+
+            let mut rx_time = None;
+            let mut gro = None;
+
+            for cmsg in r.cmsgs() {
+                match cmsg {
+                    ControlMessageOwned::RxqOvfl(c) =>
+                        println!("TODO: udp packet drop count metrics? // {c}"),
+                    ControlMessageOwned::ScmTimestampns(val) => {
+                        rx_time = SystemTime::UNIX_EPOCH.checked_add(val.into());
+                    },
+                    ControlMessageOwned::UdpGroSegments(val) => gro = Some(val),
+                    _ => return Err(Errno::EINVAL.into()),
+                }
+            }
+
+            Ok(RecvData {
+                peer_addr,
+                bytes,
+                gro,
+                rx_time,
+            })
+        },
+        Err(e) => Err(e),
+    }
 }
 
 #[cfg(test)]
