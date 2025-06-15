bdaef5eec0ffc7618fbe21fe52edddbce41f9781
Florentin Rochet <florentin.rochet@unamur.be>
2025-06-15 15:18:39 +0200

    fix attempt #12

--- apps/src/bin/quiceh-server.rs
+++ apps/src/bin/quiceh-server.rs
@@ -616,7 +616,7 @@ fn main() {
             }
 
             if total_write == 0 || dst_info.is_none() {
-                break;
+                continue;
             }
 
             let transmit = Transmit {
